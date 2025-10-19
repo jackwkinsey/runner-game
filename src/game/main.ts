7dade80907112afa4a48f3ec58277aab11933afe
Jack Kinsey <jack.w.kinsey@gmail.com>
2025-10-19 17:53:39 -0400

    refactor: adjust game configuration and scene management, including updated dimensions and physics settings

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,19 +1,28 @@
-import { Boot } from './scenes/Boot'
-import { GameOver } from './scenes/GameOver'
-import { Game as MainGame } from './scenes/Game'
-import { MainMenu } from './scenes/MainMenu'
+import GameOver from './scenes/GameOver'
+import MainGame from './scenes/Game'
 import { AUTO, Game } from 'phaser'
-import { Preloader } from './scenes/Preloader'
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
+  width: 600,
+  height: 300,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  autoRound: false,
   parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 350 },
+      // debug: true,
+    },
+  },
   backgroundColor: '#028af8',
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  scene: [MainGame, GameOver],
 }
 
 const StartGame = (parent: string) => {
