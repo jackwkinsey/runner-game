3e4089b411c5e161e37033433568da04fb6f57d2
Jack Kinsey <jack.w.kinsey@gmail.com>
2025-10-19 17:53:31 -0400

    refactor: update GameOver scene to improve structure and visuals, including background color and text display

--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -1,33 +1,38 @@
-import { Scene } from 'phaser'
+import Phaser from 'phaser'
 
-export class GameOver extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera
-  background: Phaser.GameObjects.Image
-  gameover_text: Phaser.GameObjects.Text
+class GameOver extends Phaser.Scene {
+  width: number
+  height: number
+  center_width: number
+  center_height: number
 
   constructor() {
-    super('GameOver')
+    super({ key: 'gameover' })
   }
 
   create() {
-    this.camera = this.cameras.main
-    this.camera.setBackgroundColor(0xff0000)
+    this.width =
+      typeof this.sys.game.config.width === 'string' ? parseInt(this.sys.game.config.width) : this.sys.game.config.width
+    this.height =
+      typeof this.sys.game.config.height === 'string'
+        ? parseInt(this.sys.game.config.height)
+        : this.sys.game.config.height
+    this.center_width = this.width / 2
+    this.center_height = this.height / 2
 
-    this.background = this.add.image(512, 384, 'background')
-    this.background.setAlpha(0.5)
+    this.cameras.main.setBackgroundColor(0x87ceeb)
 
-    this.gameover_text = this.add.text(512, 384, 'Game Over', {
-      fontFamily: 'Arial Black',
-      fontSize: 64,
-      color: '#ffffff',
-      stroke: '#000000',
-      strokeThickness: 8,
-      align: 'center',
-    })
-    this.gameover_text.setOrigin(0.5)
+    this.add.bitmapText(this.center_width, 50, 'arcade', this.registry.get('score'), 25).setOrigin(0.5)
+    this.add.bitmapText(this.center_width, this.center_height, 'arcade', 'GAME OVER', 45).setOrigin(0.5)
+    this.add.bitmapText(this.center_width, 250, 'arcade', 'Press SPACE or click to restart!', 15).setOrigin(0.5)
 
-    this.input.once('pointerdown', () => {
-      this.scene.start('MainMenu')
-    })
+    this.input.keyboard?.on('keydown-SPACE', this.startGame, this)
+    this.input.on('pointerdown', this.startGame, this)
+  }
+
+  startGame() {
+    this.scene.start('game')
   }
 }
+
+export default GameOver
